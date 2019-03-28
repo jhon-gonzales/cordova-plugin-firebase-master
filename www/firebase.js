67af400e36a323ad833b8db4f21d880e3c716d82
jgonzajo <jkevingonzales@outlook.com>
2019-03-28 15:04:28 -0500

    Update firebase.js

--- www/firebase.js
+++ www/firebase.js
@@ -193,7 +193,7 @@ exports.Reviews = function (contractnumber,calificacion, date, success, error) {
 };
 
 exports.ReviewsWithComment = function(contractnumber,calificacion, comment, date, success, error ){
-  exec(success,error, PLUGIN_NAME="writeWithComment", [calificacion,comment, date,contractnumber]);
+  exec(success,error, PLUGIN_NAME, "writeWithComment", [calificacion,comment, date,contractnumber]);
 };
 
 exports.AuthToken=function(token,success,error){
