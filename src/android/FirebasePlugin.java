e0ffd1045a8af94bd67667362de975a5fa8f130a
jgonzajo <jkevingonzales@outlook.com>
2019-04-28 21:08:58 -0500

    Update FirebasePlugin.java

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -235,7 +235,7 @@ public class FirebasePlugin extends CordovaPlugin {
   }
 
   public boolean userExist(final CallbackContext callbackContext, final String contractnumber){
-    final boolean result;
+    boolean result;
     DatabaseReference mDatabase;
       mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
       mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
