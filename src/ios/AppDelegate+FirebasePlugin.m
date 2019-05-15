0d8278b2ac6ba25101bc94f77607bdcafab750eb
jgonzajo <jkevingonzales@outlook.com>
2019-05-15 14:57:06 -0500

    Update AppDelegate+FirebasePlugin.m

--- src/ios/AppDelegate+FirebasePlugin.m
+++ src/ios/AppDelegate+FirebasePlugin.m
@@ -8,6 +8,7 @@
 @import FirebaseRemoteConfig;
 @import FirebaseAuth;
 @import Firebase;
+@import FirebaseDatabase;
 #import <objc/runtime.h>
 
 #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
