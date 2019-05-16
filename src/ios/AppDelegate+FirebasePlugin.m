4d6d7beaa629568c9e6f3a8ea7a2b9dde6664c6a
jgonzajo <jkevingonzales@outlook.com>
2019-05-16 09:57:39 -0500

    Update AppDelegate+FirebasePlugin.m

--- src/ios/AppDelegate+FirebasePlugin.m
+++ src/ios/AppDelegate+FirebasePlugin.m
@@ -8,7 +8,6 @@
 @import FirebaseRemoteConfig;
 @import FirebaseAuth;
 @import Firebase;
-@import FirebaseDatabase;
 #import <objc/runtime.h>
 
 #if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
@@ -52,13 +51,13 @@
     return objc_getAssociatedObject(self, kApplicationInBackgroundKey);
 }
 
--(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
     [self application:application didFinishLaunchingWithOptions:launchOptions];
     [FIRApp configure];
     return YES;
 }
 
-- (BOOL)application:(UIApplication *)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
     NSLog(@"FirebasePlugin - Finished launching");
     [self application:application swizzledDidFinishLaunchingWithOptions:launchOptions];
 
