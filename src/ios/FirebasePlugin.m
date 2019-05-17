0da7ff6fd47d1f969fbdd6e282d3a2e6c7f0054f
jgonzajo <jkevingonzales@outlook.com>
2019-05-17 17:12:51 -0500

    Update FirebasePlugin.m

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -1,7 +1,7 @@
 #import "FirebasePlugin.h"
 #import <Cordova/CDV.h>
 #import "AppDelegate.h"
-#import "Firebase.h"
+#import "Firebase/Firebase.h"
 @import Fabric;
 @import Crashlytics;
 @import FirebaseInstanceID;
