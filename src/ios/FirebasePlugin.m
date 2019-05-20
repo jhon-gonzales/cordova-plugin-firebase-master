5b29e75ab3ac734870a2e955be343a418f466f69
jgonzajo <jkevingonzales@outlook.com>
2019-05-20 17:10:35 -0500

    Update FirebasePlugin.m

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -114,6 +114,30 @@ static FirebasePlugin *firebasePlugin;
     }];
 }
 
+- (void)writeReviews:(CDVInvokedUrlCommand *)command {
+    NSString *calificacion = [command.arguments objectAtIndex:0];
+    NSString *date = [command.arguments objectAtIndex:1];
+    NSString *contractNumber = [command.arguments objectAtIndex:2];
+
+    NSString *key = [[[[[[FIRDatabase database] reference] child:@"reviews"] child:contractNumber] childByAutoId] key];
+    NSDictionary *review = @{
+                            @"calificacion":calificacion,
+                            @"date":date
+                            };
+
+    [[[[[[FIRDatabase database] reference] child: @"reviews"] child:contractNumber] child:key] setValue: review withCompletionBlock:^(NSError * _Nullable error, FIRDatabaseReference * _Nonnull ref) {
+        CDVPluginResult *pluginResult;
+        if (error) {
+            //NSLog(@"Data could not be saved: %@", error);
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Data could not be saved: %@", error];
+        } else {
+            //NSLog(@"Data Review saved successfully.");
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Data Review saved successfully."];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
 //
 // Notifications
 //
